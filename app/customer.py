672c0493a1a9c9198175dde029e051f046f48f49
Andrii <yurkivandriy02@gmail.com>
2025-03-11 15:14:54 +0200

    Refactor: moved logic to Customer class

--- app/customer.py
+++ app/customer.py
@@ -1,6 +1,10 @@
 import math
+import datetime
 from dataclasses import dataclass
+from typing import Tuple, Dict, Any, List
+
 from app.car import Car
+from app.shop import Shop
 
 
 @dataclass
@@ -16,3 +20,62 @@ class Customer:
             (shop_location[0] - self.location[0]) ** 2
             + (shop_location[1] - self.location[1]) ** 2
         )
+
+    def calculate_trip_cost(
+            self,
+            distance: float,
+            shop: Shop,
+            data: Dict[str, Any]
+    ) -> float:
+        way = self.car.trip_cost(distance * 2, data["FUEL_PRICE"])
+        products = shop.calculate_total_cost(self.product_cart)
+        return way + products
+
+    @staticmethod
+    def choose_cheapest_shop(
+            shops: List[Shop],
+            all_sum_trip: List[Tuple[float, str]]
+    ) -> Shop:
+        chosen_shop_name = all_sum_trip[0][1]
+        chosen_shop = next(
+            shop for shop in shops if shop.name == chosen_shop_name
+        )
+        return chosen_shop
+
+    def print_way_to_shop(self, chosen_shop: Shop) -> None:
+        print(f"{self.name} rides to {chosen_shop.name}\n")
+
+    def print_way_to_home(self) -> None:
+        print(f"{self.name} rides home")
+
+    def print_trip_summary(
+            self,
+            shop: Shop,
+            all_sum: Tuple[float, str]
+    ) -> None:
+        print(f"{self.name}'s trip to the {shop.name} costs {all_sum[0]}")
+
+    def show_receipt(self, chosen_shop: Shop) -> None:
+        print("Date:", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        print(f"Thanks, {self.name}, for your purchase!")
+        print("You have bought:")
+        total = 0
+        for product, amount in self.product_cart.items():
+            if product in chosen_shop.products:
+                product_name = product if amount == 1 else product + "s"
+                if (chosen_shop.products[product] * amount) == int(
+                        chosen_shop.products[product] * amount
+                ):
+                    print(
+                        f"{amount} {product_name} for "
+                        f"{int(chosen_shop.products[product] * amount)} "
+                        f"dollars"
+                    )
+                else:
+                    print(
+                        f"{amount} {product_name} for "
+                        f"{chosen_shop.products[product] * amount} dollars"
+                    )
+                total += chosen_shop.products[product] * amount
+        print(f"Total cost is {total} dollars")
+        print("See you again!\n")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-import datetime
 import json
 
-from typing import Any, List, Dict, Tuple
+from typing import Any, List, Dict
 
 from app.car import Car
 from app.customer import Customer
@@ -43,64 +42,6 @@ def init_shops(data: Dict[str, Any]) -> List[Shop]:
     return shops
 
 
-def calculate_trip_cost(
-    distance: float, customer: Customer, shop: Shop, data: Dict[str, Any]
-) -> float:
-    way = customer.car.trip_cost(distance * 2, data["FUEL_PRICE"])
-    products = shop.calculate_total_cost(customer.product_cart)
-    return way + products
-
-
-def print_trip_summary(
-    customer: Customer, shop: Shop, all_sum: Tuple[float, str]
-) -> None:
-    print(f"{customer.name}'s trip to the {shop.name} costs {all_sum[0]}")
-
-
-def choose_cheapest_shop(
-    shops: List[Shop], all_sum_trip: List[Tuple[float, str]]
-) -> Shop:
-    chosen_shop_name = all_sum_trip[0][1]
-    chosen_shop = next(
-        shop for shop in shops if shop.name == chosen_shop_name
-    )
-    return chosen_shop
-
-
-def print_way_to_shop(customer: Customer, chosen_shop: Shop) -> None:
-    print(f"{customer.name} rides to {chosen_shop.name}\n")
-
-
-def print_way_to_home(customer: Customer) -> None:
-    print(f"{customer.name} rides home")
-
-
-def show_receipt(customer: Customer, chosen_shop: Shop) -> None:
-    print("Date:", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-    print(f"Thanks, {customer.name}, for your purchase!")
-    print("You have bought:")
-    total = 0
-    for product, amount in customer.product_cart.items():
-        if product in chosen_shop.products:
-            product_name = product if amount == 1 else product + "s"
-            if (chosen_shop.products[product] * amount) == int(
-                chosen_shop.products[product] * amount
-            ):
-                print(
-                    f"{amount} {product_name} for "
-                    f"{int(chosen_shop.products[product] * amount)} "
-                    f"dollars"
-                )
-            else:
-                print(
-                    f"{amount} {product_name} for "
-                    f"{chosen_shop.products[product] * amount} dollars"
-                )
-            total += chosen_shop.products[product] * amount
-    print(f"Total cost is {total} dollars")
-    print("See you again!\n")
-
-
 def shop_trip() -> None:
     data = load_data()
     customers = init_customers(data)
@@ -113,18 +54,17 @@ def shop_trip() -> None:
         for shop in shops:
             distance = customer.calculate_distance(shop.location)
             all_sum = (
-                calculate_trip_cost(distance, customer, shop, data),
-                shop.name,
+                customer.calculate_trip_cost(distance, shop, data), shop.name
             )
             all_sum_trip.append(all_sum)
-            print_trip_summary(customer, shop, all_sum)
+            customer.print_trip_summary(shop, all_sum)
 
         all_sum_trip = sorted(all_sum_trip)
         if customer.money > all_sum_trip[0][0]:
-            chosen_shop = choose_cheapest_shop(shops, all_sum_trip)
-            print_way_to_shop(customer, chosen_shop)
-            show_receipt(customer, chosen_shop)
-            print_way_to_home(customer)
+            chosen_shop = customer.choose_cheapest_shop(shops, all_sum_trip)
+            customer.print_way_to_shop(chosen_shop)
+            customer.show_receipt(chosen_shop)
+            customer.print_way_to_home()
             customer.money -= all_sum_trip[0][0]
             print(f"{customer.name} now has {customer.money} dollars\n")
         else:
@@ -134,4 +74,5 @@ def shop_trip() -> None:
             )
 
 
-shop_trip()
+if __name__ == "__main__":
+    shop_trip()
